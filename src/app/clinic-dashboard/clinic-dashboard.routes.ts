bec10c3bd3901c45020e7cfa3d019e5c773fdbdb
Donald Kibet <chelashawdonald@yahoo.com>
2020-07-23 09:45:47 +0300

    Start of MF Clinical Dashboard

--- src/app/clinic-dashboard/clinic-dashboard.routes.ts
+++ src/app/clinic-dashboard/clinic-dashboard.routes.ts
@@ -1,5 +1,6 @@
 import { ClinicDashboardComponent } from './clinic-dashboard.component';
 import { ClinicDashboardGuard } from './clinic-dashboard.guard';
+import { MicroFrontendReportComponent } from './micro-frontend-report/micro-frontend-report.component';
 export const routes = [
   {
     path: '',
@@ -27,6 +28,10 @@ export const routes = [
             path: 'hemato-oncology',
             loadChildren:
               './oncology/oncology-program.module#OncologyProgramModule'
+          },
+          {
+            path: 'microfrontend-reports',
+            component: MicroFrontendReportComponent
           },
           { path: '', redirectTo: 'general', pathMatch: 'prefix' }
         ]
